41443346dccb22cc8cdd7a4f3eed8e6f88a8d470
sir <gari69wat@gmail.com>
2024-01-22 15:45:18 +0000

    databasechanges

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,9 @@
+# MySQL connection details
+MYSQL_USER="sir2code"
+MYSQL_PASSWORD="(1234567890)"
+MYSQL_HOST="New_local"
+
+# Database name
+DATABASE_NAME="hbtn_0c_0"
+
+# Execute MySQL query to delete the database
---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,16 @@
+# MySQL connection details
+MYSQL_USER="sir2code"
+MYSQL_PASSWORD="(1234567890)"
+MYSQL_HOST="New_local"
+
+# Get the database name from the command-line argument
+DATABASE_NAME="$1"
+
+# Check if the database name is provided
+if [ -z "$DATABASE_NAME" ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+# Execute MySQL query to list all tables in the specified database
+mysql -h"${MYSQL_HOST}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "USE ${DATABASE_NAME}; SHOW TABLES;"

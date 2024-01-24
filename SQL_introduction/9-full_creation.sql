6a65289d5f3b624089b588ad0309803ced93299e
sir <gari69wat@gmail.com>
2024-01-24 19:29:42 +0000

    insert

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,20 +1,14 @@
 -- full creation on mysql
-# MySQL connection details
-MYSQL_USER="sir2code"
-MYSQL_PASSWORD="(1234567890)"
-MYSQL_HOST="New_local"
+USE hbtn_0c_0;
 
-# Get the database name from the command-line argument
-DATABASE_NAME="$1"
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
 
-# Check if the database name is provided
-if [ -z "$DATABASE_NAME" ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-# Execute MySQL query to create the table second_table if it doesn't exist
-mysql -h"${MYSQL_HOST}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "USE ${DATABASE_NAME}; CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);"
-
-# Execute MySQL queries to add multiple rows to second_table
-mysql -h"${MYSQL_HOST}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "USE ${DATABASE_NAME}; INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);"
+-- Inserting multiple rows
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);

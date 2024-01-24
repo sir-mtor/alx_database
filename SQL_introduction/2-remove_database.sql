083ae0106de8b9c88adda24f84acdc0bd7af6e04
sir <gari69wat@gmail.com>
2024-01-24 18:41:29 +0000

    removing

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,10 +1,2 @@
 -- remove database
-# MySQL connection details
-MYSQL_USER="sir2code"
-MYSQL_PASSWORD="(1234567890)"
-MYSQL_HOST="New_local"
-
-# Database name
-DATABASE_NAME="hbtn_0c_0"
-
-# Execute MySQL query to delete the database
+DROP DATABASE IF EXISTS hbtn_0c_0;

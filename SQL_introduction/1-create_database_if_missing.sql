6970b6a8b0f1efd1775ccfde714af9abc83aa903
sir <gari69wat@gmail.com>
2024-01-24 18:39:19 +0000

    create

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,11 +1,2 @@
 -- intro to creating database on sql
-# MySQL connection details
-MYSQL_USER="sir2code"
-MYSQL_PASSWORD="(1234567890)"
-MYSQL_HOST="New_local"
-
-# Database name
-DATABASE_NAME="hbtn_0c_0"
-
-# Execute MySQL query to create the database
-mysql -h New_local -P 3306 -u sir2code -p'(1234567890)'"-e "CREATE DATABASE IF NOT EXISTS ${"hbtn_0c_0"};"
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

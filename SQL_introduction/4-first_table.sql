f437a2391936593452b2c52130d658a2e3ee215e
sir <gari69wat@gmail.com>
2024-01-24 19:07:36 +0000

    table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,17 +1,5 @@
 -- my first table on mysql
-# MySQL connection details
-MYSQL_USER="sir2code"
-MYSQL_PASSWORD="(1234567890)"
-MYSQL_HOST="New_local"
-
-# Get the database name from the command-line argument
-DATABASE_NAME="$1"
-
-# Check if the database name is provided
-if [ -z "$DATABASE_NAME" ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-# Execute MySQL query to create the table first_table if it doesn't exist
-mysql -h"${MYSQL_HOST}" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "USE ${DATABASE_NAME}; CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));"
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);

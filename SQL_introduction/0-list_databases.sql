9c3debc0d28d82a3a0f7a48343a62f6a8932a9a4
sir <gari69wat@gmail.com>
2024-01-24 17:34:07 +0000

    ml

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,9 +1,4 @@
 -- say something 
-import list_database.sh
-# MySQL connection details
-MYSQL_USER="sir2code"
-MYSQL_PASSWORD="(1234567890)"
-MYSQL_HOST="New_local"
+-- 0-list_databases.sql
 
-# Execute MySQL query to list databases
-mysql -h New_local -P 3306 -u sir2code -p'(1234567890)'"
+SHOW DATABASES;

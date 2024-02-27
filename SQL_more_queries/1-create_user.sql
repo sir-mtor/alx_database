12a6f9a141c09e2033298dff8b3046cb75bd189a
sir <gari69wat@gmail.com>
2024-02-27 09:47:18 +0000

    user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- root project
-SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwd';
 SELECT host, user, authentication_string FROM mysql.user WHERE user = 'user_0d_1';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
